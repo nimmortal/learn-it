c9ad62263c1d9fdecd85d5d208e5b58a1deff404
andrejevs <nimmortalz@gmail.com>
2018-04-20 08:11:53 +0300

    Spring Webflux - annotation project setup

---
+++ spring-webflux/webflux-annotations/src/main/java/it/learn/webfluxannotations/WebfluxAnnotationsApplication.java
@@ -0,0 +1,12 @@
+package it.learn.webfluxannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxAnnotationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxAnnotationsApplication.class, args);
+    }
+}
