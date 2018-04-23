0a94a36c3fde70a6bfe2530a528947a065142970
andrejevs <nimmortalz@gmail.com>
2018-04-23 08:29:12 +0300

    Spring Webflux - functional chaining router and nesting router

--- spring-webflux/webflux-functional/src/main/java/it/learn/webfluxfunctional/WebfluxFunctionalApplication.java
+++ spring-webflux/webflux-functional/src/main/java/it/learn/webfluxfunctional/WebfluxFunctionalApplication.java
@@ -1,13 +1,23 @@
 package it.learn.webfluxfunctional;
 
 import it.learn.webfluxfunctional.product.Product;
+import it.learn.webfluxfunctional.product.ProductHandler;
 import it.learn.webfluxfunctional.product.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @SpringBootApplication
 public class WebfluxFunctionalApplication {
@@ -36,6 +46,31 @@ public class WebfluxFunctionalApplication {
 					.thenMany(repository.findAll())
 					.subscribe(System.out::println);*/
         };
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> routes(ProductHandler handler) {
+//		return route(GET("/products").and(accept(APPLICATION_JSON)), handler::getAllProducts)
+//				.andRoute(POST("/products").and(contentType(APPLICATION_JSON)), handler::saveProduct)
+//				.andRoute(DELETE("/products").and(accept(APPLICATION_JSON)), handler::deleteAllProducts)
+//				.andRoute(GET("/products/events").and(accept(TEXT_EVENT_STREAM)), handler::getProductEvents)
+//				.andRoute(GET("/products/{id}").and(accept(APPLICATION_JSON)), handler::getProduct)
+//				.andRoute(PUT("/products/{id}").and(contentType(APPLICATION_JSON)), handler::updateProduct)
+//				.andRoute(DELETE("/products/{id}").and(accept(APPLICATION_JSON)), handler::deleteProduct);
+
+        return nest(path("/products"),
+                nest(accept(APPLICATION_JSON).or(contentType(APPLICATION_JSON)).or(accept(TEXT_EVENT_STREAM)),
+                        route(GET("/"), handler::getAllProducts)
+                                .andRoute(method(HttpMethod.POST), handler::saveProduct)
+                                .andRoute(DELETE("/"), handler::deleteAllProducts)
+                                .andRoute(GET("/events"), handler::getProductEvents)
+                                .andNest(path("/{id}"),
+                                        route(method(HttpMethod.GET), handler::getProduct)
+                                                .andRoute(method(HttpMethod.PUT), handler::updateProduct)
+                                                .andRoute(method(HttpMethod.DELETE), handler::deleteProduct)
+                                )
+                )
+        );
     }
 
 }
