c5748326ba78563f19d797732340cac986f3ae9c
andrejevs <nimmortalz@gmail.com>
2018-06-14 08:51:25 +0300

    [Spring Boot] setup project

---
+++ spring-boot/configuration-props/src/main/java/it/learn/configurationprops/ConfigurationPropsApplication.java
@@ -0,0 +1,12 @@
+package it.learn.configurationprops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationPropsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationPropsApplication.class, args);
+    }
+}
